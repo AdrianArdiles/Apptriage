1751e03b5b8fed5274b2b68e4accc9182aaa11ab
AdrianArdiles <adrianadroco@gmail.com>
2026-02-28 22:02:43 -0300

    Configuración final de CORS y seguridad para App Móvil

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,14 @@ const config: CapacitorConfig = {
   appName: 'TriajeMedico',
   webDir: 'out',
   server: {
-    androidScheme: 'http', // Esto es vital para que Android acepte la conexión
+    androidScheme: 'https', // Esto es vital para que Android acepte la conexión
     cleartext: true         // Permite conexiones de prueba si algo falla
-  }
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: false, // Desactivamos la interceptación para que el fetch sea puro
+    },
+  },
 };
 
 export default config;
